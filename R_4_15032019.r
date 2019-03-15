04f6177b791df9a52a92664db51c52bda879165d
shashankadhikarla <ashashankvishwanath@gmail.com>
2019-03-15 22:12:05 +0530

    Day 1 Code 4 ANDC R Workshop (15032019)

---
+++ R_4_15032019.r
@@ -0,0 +1,25 @@
+guest@AND010:~/.mozilla/firefox/Learning-R$ git add . # The fullstop at the end allows us to commit ALL changes that have been made to the said directory in ONE go. Useful when a lot of additions and commits have to be made. 
+gitguest@AND010:~/.mozilla/firefox/Learning-R$ git commit -m "Day 1 Code 3 ANDC Rrkshop (15032019)" #Commit as usual
+[master 4175ad6] Day 1 Code 3 ANDC R Workshop (15032019) # Result display screen; Tells all the changes that have been made till now
+ 3 files changed, 101 insertions(+), 1 deletion(-)
+ create mode 100644 R_3_15032019.r
+guest@AND010:~/.mozilla/firefox/Learning-R$ git push orgin master 
+
+
+# Back to Matrix> mat5=matrix(c(1,2,3,4), ncol=2)
+
+> matx = matrix(c(1,2,3,4), ncol=2) # Making a 2x2 matrix
+> matx
+     [,1] [,2]
+[1,]    1    3
+[2,]    2    4
+
+> matx[2,2] # The square brackets are used to display the elements. [2,2] displays the 2,2 element of the matrix ie; displays 4.
+[1] 4
+
+
+
+
+
+
+
