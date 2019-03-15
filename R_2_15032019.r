1802bea5819c3f8232a41f6076ada7502a026b32
shashankadhikarla <ashashankvishwanath@gmail.com>
2019-03-15 21:26:25 +0530

    Day 1 Code 2 ANDC R Workshop (15032019)

---
+++ R_2_15032019.r
@@ -0,0 +1,43 @@
+# Continuation of R_1_15032019 
+# We shall be adding, committing and pushing to GitHub 
+
+guest@AND010:~/.mozilla/firefox/Learning-R$ git add R_1_15032019.r # Git Add 
+guest@AND010:~/.mozilla/firefox/Learning-R$ git commit -m "Day 1 Code 1 ANDC R Workshop (15032019)" # Git Commit 
+
+*** Please tell me who you are. # Lol; Configuration needs to be done. 
+
+Run
+
+  git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
+
+guest@AND010:~/.mozilla/firefox/Learning-R$ git config --global user.email "ashashankvishwanath@gmail.com" # Git ID email
+guest@AND010:~/.mozilla/firefox/Learning-R$ git config --global user.name "shashankadhikarla" # Git ID User Name
+
+# Git configuration (done!)
+
+guest@AND010:~/.mozilla/firefox/Learning-R$ git commit -m "Day 1 Code 1 ANDC R Workshop (15032019)" 
+[master (root-commit) b689838] Day 1 Code 1 ANDC R Workshop (15032019)
+ 1 file changed, 153 insertions(+)
+ create mode 100644 R_1_15032019.r
+guest@AND010:~/.mozilla/firefox/Learning-R$ 
+# After configuration, commit again
+
+guest@AND010:~/.mozilla/firefox/Learning-R$ git commit -m "Day 1 Code 1 ANDC R Workshop (15032019)" 
+[master (root-commit) b689838] Day 1 Code 1 ANDC R Workshop (15032019)
+ 1 file changed, 153 insertions(+) # Here you can see the length of the code file
+ create mode 100644 R_1_15032019.r
+guest@AND010:~/.mozilla/firefox/Learning-R$ git push origin master # Origin Master is essentially Branch 
+Username for 'https://github.com': shashankadhikarla
+Password for 'https://shashankadhikarla@github.com': # HEHEHEHE! 
+Counting objects: 3, done.
+Delta compression using up to 4 threads.
+Compressing objects: 100% (2/2), done.
+Writing objects: 100% (3/3), 2.42 KiB | 0 bytes/s, done.
+Total 3 (delta 0), reused 0 (delta 0)
+To https://github.com/shashankadhikarla/Learning-R.git
+ * [new branch]      master -> master
+# Commiting and then pushing 
+
+# We shall now add this file to GitHub as well. 
+
